b11f3808e097447b51c09b91019b087a84efd12c
Gayatri-2017 <30742345+Gayatri-2017@users.noreply.github.com>
2017-08-22 00:46:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,11 +104,25 @@ app.get('/test-db', function (req, res){
 
 
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] = {} content object for article one
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length == 0) {
+                res.status(404).send('Article Not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    res.send(createTemplate(articleData));
 });
 
 
